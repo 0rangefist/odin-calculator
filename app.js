770814f4ef2453bab4058a1f75d32d1844b230ea
Thomas <collinskthomas@gmail.com>
2022-08-21 10:55:51 +0000

    Implement subtract() function

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-let add = function(num1, num2){
-    return num1+num2;
-}
+let add = function (num1, num2) {
+  return num1 + num2;
+};
 
-let subtract = function(){
+let subtract = function (num1, num2) {
+  return num1 - num2;
+};
 
-}
+let multiply = function () {};
 
-let multiply = function(){
+let divide = function () {};
 
-}
-
-let divide = function(){
-
-}
-
-//console.log(add(-1,-2));
+// console.log(subtract(5, 2));
