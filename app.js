fff4a1334219d1a8cd7e04a9d76afde7b91bf394
Thomas <collinskthomas@gmail.com>
2022-08-21 11:01:14 +0000

    Implement divide() function

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ let subtract = function (num1, num2) {
 };
 
 let multiply = function (num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 };
 
-let divide = function () {};
+let divide = function (num1, num2) {
+  if (num2 == 0) {
+    return "LOL. Can't do that!";
+  }
+  return num1 / num2;
+};
 
-// console.log(multiply(-5, 2));
+// console.log(divide(2, 0.));
